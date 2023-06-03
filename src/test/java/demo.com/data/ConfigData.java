226a32ee75ff7cb0779faff9474a11c8ce1a3c16
germanmalykh <malykh.gn@esphere.ru>
2023-06-03 18:16:25 +0300

    project conf

--- src/test/java/demo.com/data/ConfigData.java
+++ src/test/java/demo.com/data/ConfigData.java
@@ -2,9 +2,9 @@ package demo.com.data;
 
 public class ConfigData {
 
-    public static final String
-            CONFIG_REMOTE = System.getProperty("remote"),
-            CONFIG_BROWSER_VERSION = System.getProperty("browser_version"),
-            CONFIG_LOGIN_REMOTE = "user1",
-            CONFIG_PASSWORD_REMOTE = "1234";
+//    public static final String
+//            CONFIG_REMOTE = System.getProperty("remote"),
+//            CONFIG_BROWSER_VERSION = System.getProperty("browser_version"),
+//            CONFIG_LOGIN_REMOTE = "user1",
+//            CONFIG_PASSWORD_REMOTE = "1234";
 }
