c8b42eed5a754357ac0824de007da95da7c9ec12
germanmalykh <malykh.gn@esphere.ru>
2023-06-03 17:10:36 +0300

    added config

--- src/test/java/demo.com/config/TestBase.java
+++ src/test/java/demo.com/config/TestBase.java
@@ -5,20 +5,30 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadTimeout = 60000;
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion = "100.0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
     void beforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = "https://demoqa.com";
     }
 }
