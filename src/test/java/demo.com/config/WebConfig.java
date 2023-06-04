ac0f7324fa32798910a0d1381d594280cd006ebf
germanmalykh <malykh.gn@esphere.ru>
2023-06-04 23:03:34 +0300

    add owner conf

--- src/test/java/demo.com/config/WebConfig.java
+++ src/test/java/demo.com/config/WebConfig.java
@@ -10,9 +10,11 @@ public interface WebConfig extends Config {
     String browser();
 
     @Key("browserVersion")
+    @DefaultValue("100.0")
     String browserVersion();
 
     @Key("browserSize")
+    @DefaultValue("1920x1080")
     String browserSize();
 
     @Key("baseUrl")
